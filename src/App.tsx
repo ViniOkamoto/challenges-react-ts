c535f8ff0ee30e6d6991a253d465862df73bbfda
viniciusokamoto <leo_kamoto@hotmail.com>
2022-08-23 15:20:38 -0500

    feat(Header): move Header to components folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import { Header } from './features/header/Header'
+import { Header } from './features/components/header/Header'
 
 function App() {
   return (
