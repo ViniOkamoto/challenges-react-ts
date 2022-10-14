fdf5d2deabc071ee0180d01c544af0faf31352ba
viniciusokamoto <leo_kamoto@hotmail.com>
2022-10-14 13:01:18 -0500

    feat(Summary): integrate to API

--- dt-money/src/App.tsx
+++ dt-money/src/App.tsx
@@ -3,12 +3,15 @@ import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './core/theme/theme'
 import Router from './core/routes/Router'
 import { GlobalStyle } from './core/theme/global'
+import TransactionsContext from './contexts/TransactionsContext'
 
 export default function App() {
   return (
     <BrowserRouter>
       <ThemeProvider theme={defaultTheme}>
-        <Router />
+        <TransactionsContext>
+          <Router />
+        </TransactionsContext>
         <GlobalStyle />
       </ThemeProvider>
     </BrowserRouter>
--- dt-money/src/components/summary/index.tsx
+++ dt-money/src/components/summary/index.tsx
@@ -1,7 +1,30 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
+import { useContext } from 'react'
+import { TransactionContext } from '../../contexts/TransactionsContext'
+import { priceFormatter } from '../../core/utils/formatters'
 import { SummaryCard, SummaryContainer } from './styles'
 
 export default function Summary() {
+  const { transactions } = useContext(TransactionContext)
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === 'income') {
+        acc.income += transaction.price
+        acc.total += transaction.price
+      }
+      if (transaction.type === 'outcome') {
+        acc.outcome += transaction.price
+        acc.total -= transaction.price
+      }
+
+      return acc
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    },
+  )
   return (
     <SummaryContainer>
       <SummaryCard>
@@ -9,7 +32,7 @@ export default function Summary() {
           <span>Income</span>
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
-        <strong>R$ 17.000</strong>
+        <strong>{priceFormatter.format(summary.income)}</strong>
       </SummaryCard>
 
       <SummaryCard>
@@ -17,7 +40,7 @@ export default function Summary() {
           <span>Outcome</span>
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
-        <strong>R$ 17.000</strong>
+        <strong>{priceFormatter.format(summary.outcome)}</strong>
       </SummaryCard>
 
       <SummaryCard variant="green">
@@ -25,7 +48,7 @@ export default function Summary() {
           <span>Total</span>
           <CurrencyDollar size={32} color="#fff" />
         </header>
-        <strong>R$ 17.000</strong>
+        <strong>{priceFormatter.format(summary.total)}</strong>
       </SummaryCard>
     </SummaryContainer>
   )
