4998b67c053f91f8d0966cf5afb0a39d32fd1dd6
viniciusokamoto <leo_kamoto@hotmail.com>
2022-10-18 17:58:02 -0500

    feat(Summary): add overflow to support small devices

--- dt-money/src/components/summary/styles.ts
+++ dt-money/src/components/summary/styles.ts
@@ -1,16 +1,13 @@
 import styled, { css } from 'styled-components'
 
-export const SummaryContainer = styled.section`
-  width: 100%;
-  max-width: 1120px;
-  margin: 0 auto;
-  padding: 0 1.5rem;
+export const SummaryContainer = styled.section.attrs({ className: 'wrapper' })`
+  overflow: scroll;
 
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   gap: 2rem;
 
-  margin-top: -5rem;
+  margin-top: -5rem !important;
 `
 interface SummaryCardProps {
   variant?: 'green' | 'red'
