0a13a762f4daa49923d31b413e64fad9715bedc0
viniciusokamoto <leo_kamoto@hotmail.com>
2022-10-18 19:55:16 -0500

    feat(TransactionContext): add pagination logic

--- dt-money/src/contexts/TransactionsContext.tsx
+++ dt-money/src/contexts/TransactionsContext.tsx
@@ -2,11 +2,24 @@ import { ReactNode, useEffect, useState, useCallback } from 'react'
 import { api } from '../core/lib/axios'
 import Transaction from '../models/transaction'
 import { createContext } from 'use-context-selector'
+import { AxiosResponse } from 'axios'
 
+interface PaginationInterface {
+  current: number
+  first: number
+  next: number
+  prev: number
+  last: number
+}
+interface ApiParams {
+  q?: string
+  page?: number
+}
 interface TransactionsContextType {
   transactions: Transaction[]
-  getTransactions: (query?: string) => void
+  getTransactions: (params: ApiParams) => void
   createNewTransaction: (data: Transaction) => void
+  pagination: PaginationInterface
 }
 
 export const TransactionContext = createContext<TransactionsContextType>(
@@ -16,34 +29,90 @@ export const TransactionContext = createContext<TransactionsContextType>(
 interface TransactionContextProps {
   children: ReactNode
 }
+
 export default function TransactionsContext({
   children,
 }: TransactionContextProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([])
+  const [pagination, setPagination] = useState<PaginationInterface>({
+    current: 1,
+    first: 1,
+    next: 1,
+    prev: 1,
+    last: 1,
+  })
 
-  const getTransactions = useCallback(async (query?: string) => {
-    const response = await api.get('transactions', {
-      params: {
-        _sort: 'createdAt',
-        _order: 'desc',
-        q: query,
-      },
+  const extractPagination = useCallback(
+    (response: AxiosResponse<any, any>, currentPage: number) => {
+      const paginationObject = parseLinkHeader(response.headers.link as string)
+
+      setPagination({
+        current: currentPage,
+        first: getPageNumber(paginationObject.first),
+        next: getPageNumber(paginationObject.next),
+        prev: getPageNumber(paginationObject.prev),
+        last: getPageNumber(paginationObject.last),
+      })
+    },
+    [],
+  )
+
+  const getTransactions = useCallback(
+    async ({ q, page = 1 }: ApiParams) => {
+      const response = await api.get('transactions', {
+        params: {
+          _sort: 'createdAt',
+          _order: 'desc',
+          q,
+          _page: page,
+          _limit: 10,
+        },
+      })
+      extractPagination(response, page)
+      setTransactions(response.data)
+    },
+    [extractPagination],
+  )
+
+  function parseLinkHeader(linkHeader: string) {
+    const linkHeadersArray = linkHeader
+      .split(', ')
+      .map((header) => header.split('; '))
+    const linkHeadersMap = linkHeadersArray.map((header) => {
+      const thisHeaderRel = header[1].replace(/"/g, '').replace('rel=', '')
+      const thisHeaderUrl = header[0].slice(1, -1)
+      return [thisHeaderRel, thisHeaderUrl]
     })
-    setTransactions(response.data)
-  }, [])
+    return Object.fromEntries(linkHeadersMap)
+  }
 
+  function getPageNumber(link?: string): number {
+    if (!link) {
+      return 0
+    }
+    const params: string = link.split('?')[1]
+    const separatedParams: string[] = params.split('&')
+    const pageParam = separatedParams.find((p) => p.startsWith('_page'))
+    const pageNumber = pageParam!.split('=')[1]
+    return Number.parseInt(pageNumber)
+  }
   const createNewTransaction = useCallback(async (data: Transaction) => {
     const response = await api.post('transactions', data)
     setTransactions((state) => [response.data, ...state])
   }, [])
 
   useEffect(() => {
-    getTransactions()
+    getTransactions({})
   }, [getTransactions])
 
   return (
     <TransactionContext.Provider
-      value={{ transactions, getTransactions, createNewTransaction }}
+      value={{
+        transactions,
+        getTransactions,
+        createNewTransaction,
+        pagination,
+      }}
     >
       {children}
     </TransactionContext.Provider>
